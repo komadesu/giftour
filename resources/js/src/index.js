04db4211435d43ea73679653cbeffb573e7e8297
Hannah-86 <hana86.shinee@gmail.com>
2020-11-25 15:05:03 +0900

    setup react file system

--- resources/js/src/index.js
+++ resources/js/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function App() {
-    return <h1>Hello React!</h1>
-}
+import App from './App'
 
 if (document.getElementById('app')) {
     ReactDOM.render(<App />, document.getElementById('app'))
