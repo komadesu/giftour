bb94d718ee068911ea74fac800e863020fd2bf50
Hannah-86 <hana86.shinee@gmail.com>
2020-11-27 22:47:59 +0900

    add style to header component, fix editorconfig, app component and header component

--- resources/js/src/App.js
+++ resources/js/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Header from "./components/Header";
 
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
       <Switch>
--- resources/js/src/components/Header.js
+++ resources/js/src/components/Header.js
@@ -5,24 +5,21 @@ class Header extends React.Component {
   render() {
     return (
       <div className="header">
-        <Link className="header__logo" to="#">
+        <Link className="header__logo header__link" to="#">
           Logo
         </Link>
         <div className="header__navs">
-          <Link className="nav" to="#">
-            genre
-          </Link>
-          <Link className="nav" to="#">
+          <Link className="nav header__link" to="#">
             timeline
           </Link>
-          <Link className="nav" to="#">
+          <Link className="nav header__link" to="#">
             My page
           </Link>
-          <Link className="nav" to="#">
+          <Link className="nav header__link" to="#">
             contact
           </Link>
         </div>
-        <Link className="header__login" to="#">
+        <Link className="header__login header__link" to="#">
           Log in
         </Link>
       </div>
