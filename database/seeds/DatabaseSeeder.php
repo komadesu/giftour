a2d2f55d1ccea7d73a88063e674f622d9ee36b1c
komadesu <happykoma777@gmail.com>
2020-11-28 00:09:16 +0900

    create Situation Model defining columns in migration and seeding fake data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(GiftsTableSeeder::class);
         // $this->call(CategoriesTableSeeder::class);
         // $this->call(ImagesTableSeeder::class);
-        $this->call(RelationshipsTableSeeder::class);
+        // $this->call(RelationshipsTableSeeder::class);
+        $this->call(SituationsTableSeeder::class);
     }
 }
