b2dbdca17e130ba2d18af8795d94fd4cd1c99043
komadesu <happykoma777@gmail.com>
2020-11-27 23:53:33 +0900

    create Image model seeding data with factory and defining columns in migration

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
         // $this->call(GiftsTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
+        // $this->call(CategoriesTableSeeder::class);
+        $this->call(ImagesTableSeeder::class);
     }
 }
