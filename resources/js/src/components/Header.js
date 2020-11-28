e9607e62abd144987b6b551b8da858162617b09b
Hannah-86 <hana86.shinee@gmail.com>
2020-11-28 13:53:01 +0900

    update

--- resources/js/src/components/Header.js
+++ resources/js/src/components/Header.js
@@ -1,30 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div className="header">
-        <Link className="header__logo header__link" to="#">
-          Logo
+const Header = () => {
+  return (
+    <div className="header">
+      <Link className="header__logo header__link" to="#">
+        Logo
+      </Link>
+      <div className="header__navs">
+        <Link className="nav header__link" to="#">
+          timeline
         </Link>
-        <div className="header__navs">
-          <Link className="nav header__link" to="#">
-            timeline
-          </Link>
-          <Link className="nav header__link" to="#">
-            My page
-          </Link>
-          <Link className="nav header__link" to="#">
-            contact
-          </Link>
-        </div>
-        <Link className="header__login header__link" to="#">
-          Log in
+        <Link className="nav header__link" to="#">
+          My page
+        </Link>
+        <Link className="nav header__link" to="#">
+          contact
         </Link>
       </div>
-    );
-  }
-}
+      <Link className="header__login header__link" to="#">
+        Log in
+      </Link>
+    </div>
+  );
+};
 
 export default Header;
