b1d97f480489c7993a20a304c8015877267c3b2e
Hannah-86 <hana86.shinee@gmail.com>
2020-12-02 21:50:58 +0900

    add style to giveform component and getform component

--- resources/js/src/components/Post.js
+++ resources/js/src/components/Post.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import GiveForm from "./GiveForm";
-import GetForm from "./GetForm";
+// import GetForm from "./GetForm";
 
 class Post extends React.Component {
   render() {
@@ -27,7 +27,7 @@ class Post extends React.Component {
           <span>a present</span>
         </div>
         <GiveForm />
-        <GetForm />
+        {/* <GetForm /> */}
       </div>
     );
   }
