253841ea65e9158065a8033ce01c92d41eb8bd92
Hannah-86 <hana86.shinee@gmail.com>
2020-12-12 00:15:53 +0900

    complete style of settings and mypage navs and implement smooth scroll

--- resources/js/src/components/MyPage.js
+++ resources/js/src/components/MyPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import _ from "lodash";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import { HashLink, NavHashLink } from "react-router-hash-link";
 
 import Gift from "./Gift";
 import { readGifts } from "../actions/gifts";
@@ -20,58 +21,104 @@ class MyPage extends React.Component {
     this.props.readUser();
   }
 
+  scrollWidthOffset(el) {
+    const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
+    const yOffset = -120;
+    window.scrollTo({ top: yCoordinate + yOffset, behavior: "smooth" });
+  }
+
   render() {
     const { user } = this.props;
     return (
       <div className="mypage">
-        <div className="mypage__archive mypage__area">
-          <h4 className="title">Archive</h4>
-          <div className="gifts">
-            <Gift gift={this.props.gifts[1]} bookmarks={this.props.bookmarks} />
-            <Gift gift={this.props.gifts[2]} bookmarks={this.props.bookmarks} />
-            <div className="more">
-              <Link className="more-btn" to="#">
-                +more
-              </Link>
+        <div className="mypage__navs">
+          <NavHashLink
+            smooth
+            className="mypage__nav"
+            to="/MyPage#archive"
+            scroll={this.scrollWidthOffset}
+          >
+            Archive
+          </NavHashLink>
+          <NavHashLink
+            smooth
+            className="mypage__nav"
+            to="/MyPage#saved"
+            scroll={this.scrollWidthOffset}
+          >
+            Saved
+          </NavHashLink>
+          <NavHashLink
+            smooth
+            className="mypage__nav"
+            to="/MyPage#settings"
+            scroll={this.scrollWidthOffset}
+          >
+            Settings
+          </NavHashLink>
+        </div>
+        <div className="mypage__areas">
+          <div className="area" id="archive">
+            <h4 className="title">Archive</h4>
+            <div className="gifts">
+              <Gift
+                gift={this.props.gifts[1]}
+                bookmarks={this.props.bookmarks}
+              />
+              <Gift
+                gift={this.props.gifts[2]}
+                bookmarks={this.props.bookmarks}
+              />
+              <div className="more">
+                <Link className="more-btn" to="#">
+                  +more
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="mypage__saved mypage__area">
-          <h4 className="title">Saved</h4>
-          <div className="gifts">
-            <Gift
-              gift={this.props.bookmarks[0]}
-              bookmarks={this.props.bookmarks}
-            />
-            <Gift
-              gift={this.props.bookmarks[1]}
-              bookmarks={this.props.bookmarks}
-            />
-            <div className="more">
-              <Link className="more-btn" to="#">
-                +more
-              </Link>
+          <div className="area" id="saved">
+            <h4 className="title">Saved</h4>
+            <div className="gifts">
+              <Gift
+                gift={this.props.bookmarks[0]}
+                bookmarks={this.props.bookmarks}
+              />
+              <Gift
+                gift={this.props.bookmarks[1]}
+                bookmarks={this.props.bookmarks}
+              />
+              <div className="more">
+                <Link className="more-btn" to="#">
+                  +more
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="mypage__settings mypage__area">
-          <h4 className="title">Settings</h4>
-          <div className="user-infos">
-            <div className="user-info name">
-              <span className="label">Name</span>
-              <span className="content">{user.name}</span>
-            </div>
-            <div className="user-info gender">
-              <span className="label">Gender</span>
-              <span className="content">{user.gender}</span>
-            </div>
-            <div className="user-info age">
-              <span className="label">Age</span>
-              <span className="content">{user.age}</span>
-            </div>
-            <div className="user-info email">
-              <span className="label">Email</span>
-              <span className="content">{user.email}</span>
+          <div className="area" id="settings">
+            <h4 className="title">Settings</h4>
+            <div className="user-infos-wrapper">
+              <div className="user-infos">
+                <div className="user-info name">
+                  <span className="label">Name</span>
+                  <span className="content text-truncate">{user.name}</span>
+                </div>
+                <div className="user-info gender">
+                  <span className="label">Gender</span>
+                  <span className="content">{user.gender}</span>
+                </div>
+                <div className="user-info age">
+                  <span className="label">Age</span>
+                  <span className="content">{user.age}</span>
+                </div>
+                <div className="user-info email">
+                  <span className="label">Email</span>
+                  <span className="content text-truncate">{user.email}</span>
+                </div>
+                <div className="btn-wrapper">
+                  <input className="btn btn__reverse" type="button" value="Edit" />
+                </div>
+              </div>
+              <div className="logo">GIFTOUR</div>
             </div>
           </div>
         </div>
