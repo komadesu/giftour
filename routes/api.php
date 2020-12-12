ad0a97564809a3cbdf945333c350556058348c1c
komadesu <happykoma777@gmail.com>
2020-12-12 17:52:39 +0900

    refactor apis with proper auth guard

--- routes/api.php
+++ routes/api.php
@@ -13,25 +13,26 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::group(['middleware' => ['api']], function() {
+Route::group(['middleware' => ['api']], function () {
   Route::post('users', 'Api\UserController@store');
 
-
-  Route::get('bookmarks/{user_id}', 'Api\BookmarkController@index');
-  Route::post('bookmarks', 'Api\BookmarkController@store');
-  Route::delete('bookmarks', 'Api\BookmarkController@destroy');
-  // Route::resource('bookmarks', 'Api\BookmarkController')->except(['create', 'show', 'edit', 'update']);
+  Route::get('/gifts', 'Api\GiftController@index');
 
 
-  Route::get('gifts', 'Api\GiftController@index');
-  Route::get('gifts/{user_id}', 'Api\GiftController@getUserPosts');
-  Route::get('gifts/{user_id}/bookmarks', 'Api\GiftController@getUserBookmarks');
-  Route::post('gifts', 'Api\GiftController@store');
-  Route::put('gifts/{gift_id}', 'Api\GiftController@update');
-  Route::delete('gifts/{gift_id}', 'Api\GiftController@destroy');
-  //Route::resource('gifts', 'Api\GiftController')->except(['create', 'show', 'edit']);
+  Route::middleware('auth:api')->group(function () {
+    Route::get('/user', function (Request $request) {
+      return $request->user();
+    });
+
+    Route::get('/bookmarks/{user_id}', 'Api\BookmarkController@index');
+    Route::post('/bookmarks', 'Api\BookmarkController@store');
+    Route::delete('/bookmarks', 'Api\BookmarkController@destroy');
+
+
+    Route::get('/gifts/{user_id}', 'Api\GiftController@getUserPosts');
+    Route::post('/gifts', 'Api\GiftController@store');
+    Route::put('/gifts/{gift_id}', 'Api\GiftController@update');
+    Route::delete('/gifts/{gift_id}', 'Api\GiftController@destroy');
+  });
+
 });
