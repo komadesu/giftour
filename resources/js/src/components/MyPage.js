2f911ea85f401e36d370f2bf991856e84345bf12
komadesu <happykoma777@gmail.com>
2020-12-13 16:47:43 +0900

    implement toggle of settings form managing each form state in mypage component that is called react controlled component

--- resources/js/src/components/MyPage.js
+++ resources/js/src/components/MyPage.js
@@ -11,6 +11,18 @@ import { readUser } from "../actions/user";
 class MyPage extends React.Component {
   constructor(props) {
     super(props);
+    this.toggleFormState = this.toggleFormState.bind(this);
+    this.onChangeName = this.onChangeName.bind(this);
+    this.onChangeGender = this.onChangeGender.bind(this);
+    this.onChangeAge = this.onChangeAge.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.state = {
+      formState: false,
+      name: this.props.user.name,
+      radio: null,
+      age: this.props.user.age,
+      email: this.props.user.email,
+    };
   }
 
   componentDidMount() {
@@ -18,8 +30,40 @@ class MyPage extends React.Component {
     this.props.readGifts();
     this.props.readBookmarks(userId);
     this.props.readUser();
+
+    const { gender } = this.props.user
+    this.setGenderState(gender)
   }
 
+  setGenderState(value) {
+    let genderState
+    if (value === 'male') genderState = 1
+    if (value === 'female') genderState = 2
+    if (value === 'other') genderState = 3
+    this.setState({ radio: genderState })
+  }
+
+  onChangeName(e) {
+    this.setState({ name: e.target.value })
+  }
+  onChangeGender(e) {
+    const gender = e.target.id
+    this.setGenderState(gender)
+  }
+  onChangeAge(e) {
+    this.setState({ age: e.target.value })
+  }
+  onChangeEmail(e) {
+    this.setState({ email: e.target.value })
+  }
+
+  toggleFormState() {
+    const { formState } = this.state;
+    const nextFormState = !formState ? true : false;
+    this.setState({
+      formState: nextFormState,
+    });
+  }
   scrollWidthOffset(el) {
     const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
     const yOffset = -120;
@@ -28,6 +72,8 @@ class MyPage extends React.Component {
 
   render() {
     const { user } = this.props;
+    const { formState } = this.state;
+
     return (
       <div className="mypage">
         <div className="mypage__navs">
@@ -90,10 +136,13 @@ class MyPage extends React.Component {
               </div>
             </div>
           </div>
+
           <div className="area" id="settings">
             <h4 className="title">Settings</h4>
+
             <div className="user-infos-wrapper">
-              <div className="user-infos">
+
+              <div className={formState ? "user-infos" : "user-infos display"}>
                 <div className="user-info name">
                   <span className="label">Name</span>
                   <span className="content text-truncate">{user.name}</span>
@@ -111,7 +160,71 @@ class MyPage extends React.Component {
                   <span className="content text-truncate">{user.email}</span>
                 </div>
                 <div className="btn-wrapper">
-                  <input className="btn btn__reverse" type="button" value="Edit" />
+                  <input
+                    className="btn btn__reverse"
+                    type="button"
+                    value="Edit"
+                    onClick={this.toggleFormState}
+                  />
+                </div>
+              </div>
+
+              <div className={formState ? "user-infos display" : "user-infos"}>
+                <div className="user-info name">
+                  <label htmlFor="form-name" className="label">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="form-name"
+                    className="content text-truncate"
+                    onChange={this.onChangeName}
+                  />
+                </div>
+                <div className="user-info gender">
+                  <span className="label">Gender</span>
+                  <div className="radio-content">
+                    <input
+                      type="radio"
+                      id="male"
+                      name="gender"
+                      checked={this.state.radio === 1}
+                      onChange={this.onChangeGender}
+                    />
+                    <label htmlFor="male">Male</label>
+                    <input
+                      type="radio"
+                      id="female"
+                      name="gender"
+                      checked={this.state.radio === 2}
+                      onChange={this.onChangeGender}
+                    />
+                    <label htmlFor="female">Female</label>
+                    <input
+                      type="radio"
+                      id="other"
+                      name="gender"
+                      checked={this.state.radio === 3}
+                      onChange={this.onChangeGender}
+                    />
+                    <label htmlFor="other">Other</label>
+                    </div>
+                </div>
+                <div className="user-info age">
+                  <label htmlFor="form-age" className="label">Age</label>
+                  <input type="number" id="form-age" className="content" onChange={this.onChangeAge} />
+                </div>
+                <div className="user-info email">
+                  <label htmlFor="form-email" className="label">Email</label>
+                  <input type="email" id="form-email" className="content text-truncate" onChange={this.onChangeEmail} />
+                </div>
+                <div className="btn-wrapper">
+                  <input
+                    className="btn btn__reverse"
+                    type="button"
+                    value="Cancel"
+                    onClick={this.toggleFormState}
+                  />
                 </div>
               </div>
               <div className="logo">GIFTOUR</div>
@@ -123,10 +236,10 @@ class MyPage extends React.Component {
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   gifts: state.gifts,
   bookmarks: state.bookmarks,
-  user: state.user
+  user: state.user,
 });
 const mapDispatchToProps = { readGifts, readBookmarks, readUser };
 export default connect(mapStateToProps, mapDispatchToProps)(MyPage);
