f72b3b54d4773a54a1fb08b24dfaeead24c0b53c
Hannah-86 <hana86.shinee@gmail.com>
2020-12-14 18:26:57 +0900

    add style to footer component

--- resources/js/src/components/Footer.js
+++ resources/js/src/components/Footer.js
@@ -1,7 +1,11 @@
-import React from "react"
+import React from "react";
 
 const Footer = () => {
-  return <div className="footer">Copyright &copy; giftour, 2020. All rights reserved.</div>;
-}
+  return (
+    <div className="footer">
+      <span>Copyright &copy; giftour, 2020. All rights reserved.</span>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
