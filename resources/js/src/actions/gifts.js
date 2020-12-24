7d2ffadadf9fe9609ea54e036afc966e269f47a0
komadesu <happykoma777@gmail.com>
2020-12-24 21:44:50 +0900

    implement ajax for fetching gifts and display those data

--- resources/js/src/actions/gifts.js
+++ resources/js/src/actions/gifts.js
@@ -1,3 +1,11 @@
+import axios from "axios";
+
+import { ROOT_URL } from "./index";
+
 export const READ_GIFTS = "READ_GIFTS";
 
-export const readGifts = () => ({ type: READ_GIFTS });
+export const readGifts = () => async dispatch => {
+  const response = await axios.get(`${ROOT_URL}api/gifts`)
+
+  dispatch({ type: READ_GIFTS, response });
+}
--- resources/js/src/reducers/gifts.js
+++ resources/js/src/reducers/gifts.js
@@ -2,57 +2,14 @@ import _ from "lodash";
 
 import { READ_GIFTS } from "../actions/gifts";
 
-const defaultGifts = [
-  {
-    id: 1,
-    name: "香水",
-    price: 4000,
-    brand: "shiro",
-    imageFileName: "preview-bg.png",
-    category: "香水・フレグランス",
-    postFlag: 1,
-    opponentGender: "male",
-    opponentAge: 41,
-    relationship: "恋人",
-    situation: "バレンタインデー",
-    createdAt: "2020-12-04 16:32:14"
-  },
-  {
-    id: 2,
-    name: "マフラー",
-    price: null,
-    brand: "ユナイテッドアローズ",
-    imageFileName: "bg-img.png",
-    category: "ファッション",
-    postFlag: 2,
-    opponentGender: "male",
-    opponentAge: 20,
-    relationship: "親子",
-    situation: "誕生日",
-    createdAt: "2020-12-04 16:32:14"
-  },
-  {
-    id: 3,
-    name: "アイシャドウパレット",
-    price: 12000,
-    brand: "Dior",
-    imageFileName: "bg-img.png",
-    category: "コスメ・健康",
-    postFlag: 1,
-    opponentGender: "female",
-    opponentAge: 32,
-    relationship: "先輩・後輩",
-    situation: "その他",
-    createdAt: "2020-12-04 16:32:14"
-  }
-];
+const defaultGifts = {}
 
 export default (gifts = defaultGifts, action) => {
   switch (action.type) {
     case READ_GIFTS:
-      return _.mapKeys(gifts, "id");
-      // return _.mapKeys(action.response.data, "id");
+      return _.mapKeys(action.response.data, "id");
     default:
       return gifts;
   }
 };
+
